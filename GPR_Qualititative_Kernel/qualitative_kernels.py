f2243324085c591323fd6abd11366db3cbab46ce
JohnHBauer <john.h.bauer@gmail.com>
2017-12-19 18:22:03 -0600

    adding docstrings, de-crufted

--- GPR_Qualititative_Kernel/qualitative_kernels.py
+++ GPR_Qualititative_Kernel/qualitative_kernels.py
@@ -10,44 +10,46 @@ from __future__ import print_function
 from hypersphere import HyperSphere
 #import hypersphere_cython as hs
 
-from math import pi, sin, cos, log, sqrt
+from math import pi, log, sqrt
 import numpy as np
-import pandas as pd
-from sklearn.gaussian_process import GaussianProcessRegressor
+# TODO: contribute back to sklearn
 #from sklearn.gaussian_process.kernels import Kernel
 #from sklearn.gaussian_process.kernels import RBF, ConstantKernel, CompoundKernel
 #from sklearn.gaussian_process.kernels import Hyperparameter
 #from sklearn.gaussian_process.kernels import NormalizedKernelMixin
 from kernels import Kernel
-from kernels import RBF, ConstantKernel, CompoundKernel
+from kernels import RBF, CompoundKernel
 from kernels import Hyperparameter
 from kernels import NormalizedKernelMixin
-from sklearn.preprocessing import StandardScaler
 from scipy.linalg import cholesky
 
 import logging
 logging.basicConfig(filename='CategoricalKernel.log',level=logging.DEBUG)
 
-
-def _check_category_scale(X, scale):
-    scale = np.squeeze(scale).astype(float)
-    if np.ndim(scale) > 1:
-        raise ValueError("scale cannot be of dimension greater than 1")
-    if np.ndim(scale) == 1 and X.shape[1] != scale.shape[0]:
-        raise ValueError("Anisotropic kernel must have the same number of "
-                         "dimensions as data (%d!=%d)"
-                         % (scale.shape[0], X.shape[1]))
-    return scale
-
+# =============================================================================
+# TODO: should this be a (Unary)KernelOperator?  If so, ExponentialKernel
+# probably should be as well...
+# =============================================================================
 class Projection(Kernel):
-    def __init__(self, columns, name="proj", kernel=None):
-        """
-        
-        kernel:     Kernel object, defaults to RBF()
-        name:       string to be used in reporting parameters
-        columns:    integer or list of integer indices of columns to project onto
-        """
-        
+    """Coordinate Projection onto a subset of the columns.
+    
+    .. versionadded:: ??
+    
+    Typically used in combination with Product, Tensor, Sum, or DirectSum.
+    For categorical variables, construct dummy-coded indicator variables,
+    use Projection onto those columns, and use ExchangeableCorrelation,
+    MultiplicativeCorrelation, or UnrestrictiveCorrelation as the kernel.
+    The resulting Projection kernel may be used in a product or tensor
+    with other kernels, such aa the projection onto the continuous variables.
+
+    Parameters
+    ----------
+    columns:    integer or list of integer indices of columns to project onto
+    name:       string to be used in reporting parameters
+    kernel:     Kernel object, defaults to RBF() with one length-scale
+                parameter for each column.
+    """
+    def __init__(self, columns, name, kernel=None):        
         if kernel is None:
             kernel = RBF([1.0] * len(columns))
         assert isinstance(kernel, Kernel), "Kernel instance required"
@@ -61,28 +63,39 @@ class Projection(Kernel):
         assert all(isinstance(i, int) for i in self.columns), "must be integers"
         
     def __call__(self, X, Y=None, eval_gradient=False):
-        # TODO: pass parameters to RBF to initialize
-        # or consider instantiating K1 outside, pass it into init
+        """Return the kernel k(X, Y) and optionally its gradient.
 
-#        X1 = pd.DataFrame(np.atleast_2d(X))[self.columns] 
-#        Y1 = pd.DataFrame(np.atleast_2d(Y))[self.columns] if Y is not None else None
+        Parameters
+        ----------
+        X : array, shape (n_samples_X, n_features)
+            Left argument of the returned kernel k(X, Y)
+            X should be the dummy-coded representation of a categorical
+            variable.  Typically used with a Projection kernel.
 
+        Y : array, shape (n_samples_Y, n_features), (optional, default=None)
+            Right argument of the returned kernel k(X, Y). If None, k(X, X)
+            if evaluated instead.  Y should be the dummy-coded representation
+            of a categorical variable.
+
+        eval_gradient : bool (optional, default=False)
+            Determines whether the gradient with respect to the kernel
+            hyperparameters is determined.
+
+        Returns
+        -------
+        K : array, shape (n_samples_X, n_samples_Y)
+            Kernel k(X, Y)
+
+        K_gradient : array, shape (n_samples_X, n_samples_X, n_dims)
+            The gradient of the kernel k(X, X) with respect to the
+            hyperparameters of the kernel. Only returned when eval_gradient
+            is True.
+        """
         X1 = np.atleast_2d(X)[:,self.columns] 
         Y1 = np.atleast_2d(Y)[:,self.columns] if Y is not None else None
         
         return self.kernel(X1, Y1, eval_gradient=eval_gradient)
     
-# =============================================================================
-#     @property
-#     def kernel(self):
-#         return self._kernel
-# =============================================================================
-
-# =============================================================================
-# Propose a UnaryOperator class to include Exponentiation kernel, 
-# Projection, SimpleCategoricalKernel, and so on
-# The sequel is copied wholesale from ExponentiationKernel
-# =============================================================================
     def get_params(self, deep=True):
         """Get parameters of this kernel.
 
@@ -97,7 +110,8 @@ class Projection(Kernel):
         params : mapping of string to any
             Parameter names mapped to their values.
         """
-        params = dict(kernel=self.kernel, columns=self.columns, name=self.name)
+        name = self.name if self.name else "proj"
+        params = dict(kernel=self.kernel, columns=self.columns, name=name)
         #params = dict(columns=self.columns)
         #name_ = "{}{}__".format(self.name, self.columns)
         if deep:
@@ -161,8 +175,9 @@ class Projection(Kernel):
     def __eq__(self, b):
         if type(self) != type(b):
             return False
-        return (self.kernel == b.kernel and self.columns == b.columns)
-
+        return (self.kernel == b.kernel and
+                self.columns == b.columns and
+                self.name == b.name)
 
     def diag(self, X):
         """Returns the diagonal of the kernel k(X, X).
@@ -195,44 +210,37 @@ class Projection(Kernel):
         return self.kernel.is_stationary()
 
 # =============================================================================
-# TODO: This is now obsolete by making RBF the default kernel of projecion
-# =============================================================================
-class Factor(Projection):
-    def __init__(self, columns, name):
-        super(Factor, self).__init__(columns,
-                                     name,
-                                     kernel=RBF([1.0] * len(columns)))
-
-    def get_params(self, deep=True):
-        """Get parameters of this kernel.
-
-        Parameters
-        ----------
-        deep: boolean, optional
-            If True, will return the parameters for this estimator and
-            contained subobjects that are estimators.
-
-        Returns
-        -------
-        params : mapping of string to any
-            Parameter names mapped to their values.
-        """
-        params = dict(columns=self.columns, name=self.name)
-        #params = dict(columns=self.columns)
-        #name_ = "{}{}__".format(self.name, self.columns)
-        if deep:
-            deep_items = self.kernel.get_params().items()
-            #params.update((name_ + k, val) for k, val in deep_items)
-            #params.update(("kernel__{}".format(k), val) for k, val in deep_items)
-            params.update(("{}__{}".format(self.name, k), val) for k, val in deep_items)
-        return params
-
-# =============================================================================
 # Assumes X includes a factor f which has been dummy coded into columns
 # F = (f_0, f_1, ... f_dim-1)
 # Use with Projection to extract the columns
 # =============================================================================
 class  UnrestrictiveCorrelation(NormalizedKernelMixin, Kernel):
+    """Unrestrictive Correlation kernel for use with qualitative factors.
+
+    Uses a hypersphere coordinate system to model the correlations
+    between categories.  Typically, a categorical variate with (dim) levels
+    will be dummy-coded into (dim) columns of indicator variables with
+    sklearn.preprocessing.OneHotEncoder, or pandas.get_dummies.
+    A Projection kernel 
+
+    k(x_i, x_j) = exp(-1 / 2 d(x_i / length_scale, x_j / length_scale)^2)
+
+    This kernel is infinitely differentiable, which implies that GPs with this
+    kernel as covariance function have mean square derivatives of all orders,
+    and are thus very smooth.
+
+    .. versionadded:: 0.18
+
+    Parameters
+    -----------
+    dim : int
+        The dimension of the factor, equal to the number of categories.
+
+    zeta:
+    length_scale_bounds : pair of floats >= 0, default: (1e-5, 1e5)
+        The lower and upper bound on length_scale
+
+    """
     def __init__(self, dim, zeta=[], zeta_bounds=(0.0, pi)): # add 2*pi in hopes of eliminating difficulties with log transform
         # TODO: fix this so zeta can be a list or array
         self.dim = dim
@@ -248,6 +256,35 @@ class  UnrestrictiveCorrelation(NormalizedKernelMixin, Kernel):
         #self.corr = self.hs.correlation
 
     def __call__(self, X, Y=None, eval_gradient=False):
+        """Return the kernel k(X, Y) and optionally its gradient.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples_X, n_features)
+            Left argument of the returned kernel k(X, Y)
+            X should be the dummy-coded representation of a categorical
+            variable.  Typically used with a Projection kernel.
+
+        Y : array, shape (n_samples_Y, n_features), (optional, default=None)
+            Right argument of the returned kernel k(X, Y). If None, k(X, X)
+            if evaluated instead.  Y should be the dummy-coded representation
+            of a categorical variable.
+
+        eval_gradient : bool (optional, default=False)
+            Determines whether the gradient with respect to the kernel
+            hyperparameters is determined.
+
+        Returns
+        -------
+        K : array, shape (n_samples_X, n_samples_Y, n_kernels)
+            Kernel k(X, Y)
+
+        K_gradient : array, shape (n_samples_X, n_samples_X, n_dims, n_kernels)
+            The gradient of the kernel k(X, X) with respect to the
+            hyperparameters of the kernel. Only returned when eval_gradient
+            is True.
+        """
+        
         #logging.debug("Factor: evaluate kernel for zeta:\n{}".format(self.zeta))
         #print("Factor: evaluate kernel for zeta:\n{}".format(self.zeta))
         assert X.shape[1] == self.dim, "Wrong dimension for X"
@@ -283,38 +320,18 @@ class  UnrestrictiveCorrelation(NormalizedKernelMixin, Kernel):
         #return self.hs.correlation
         return self.hypersphere.correlation
         
-    def naive__call__(self, X, Y=None, eval_gradient=False):
-        # retain for testing purposes
-        N = X.shape[0]
-        Y = Y if Y is not None else X
-        M = Y.shape[0]
-        K = np.zeros((N,M), dtype=np.float64)
-        corr = self.correlation
-        # for correctness use naive implementation
-        # TODO: use broadcasting or kronecker product, verify against naive
-        c = X[:,self.column]
-        for i in range(N):
-            for j in range(M):
-                K[i,j] = corr[c[i],c[j]]
-        # TODO: the gradient is NOT CORRECT it needs to be 'stretched'
-        # see __call__ for full (large!) gradient
-        if eval_gradient:
-            print("Naive Gradient is for debugging only")
-            G = self.hypersphere.gradient()
-            return K, G
-        else:
-            return K
-        
-# =============================================================================
-#     def diag(self, X):
-#         return np.ones([X.shape[0]], dtype=np.float64)
-# =============================================================================
-
     def is_stationary(self):
         return False
     
     @property
     def hyperparameter_zeta(self):
+        """Returns a list of all hyperparameters.
+        
+        For the Unrestrictive Correlation model of dimension d, there are 
+        d * (d - 1) /2 parameters bounded by (0, pi), which are the coordinates
+        of the surface of a hypersphere, one for each of the below-diagonal
+        elements of a d * d matrix."""
+
         #n_elts = len(self.zeta) if np.iterable(self.zeta) else 1
         dim = self.dim
         m = dim * (dim - 1) // 2
@@ -325,6 +342,9 @@ class  UnrestrictiveCorrelation(NormalizedKernelMixin, Kernel):
                                log=False)
 
 class ExchangeableCorrelation(Kernel):
+    """The d * d correlation matrix C is modelled by a single correlation.
+    
+    C[i,j] = zeta (i != j), C[i,i] = 1"""
     def __init__(self, dim, zeta, zeta_bounds=(0.0, 1.0)):
         self.dim = dim
         self.zeta = zeta
@@ -399,6 +419,9 @@ class ExchangeableCorrelation(Kernel):
         
     @property
     def hyperparameter_zeta(self):
+        """Single correlation parameter.
+        
+        Bounded between 0 and 1, not log-transformed."""
         return  Hyperparameter(name="zeta", 
                                value_type="numeric", 
                                bounds=self.zeta_bounds, 
@@ -468,51 +491,37 @@ class MultiplicativeCorrelation(NormalizedKernelMixin, Kernel):
 
     @property
     def correlation(self):
+        """Correlation parameterized as: C[i,j] = exp(-(zeta[i]+zeta[j])"""
         # zeta is a vector of length dim
         c = -self.zeta
         C = np.exp(c[:, np.newaxis] + c[np.newaxis, :])
         np.fill_diagonal(C, 1.0)
         return C
-# =============================================================================
-#     def diag(self, X):
-#         """Returns the diagonal of the kernel k(X, X).
-# 
-#         The result of this method is identical to np.diag(self(X)); however,
-#         it can be evaluated more efficiently since only the diagonal is
-#         evaluated.
-# 
-#         Parameters
-#         ----------
-#         X : array, shape (n_samples_X, n_features)
-#             Left argument of the returned kernel k(X, Y)
-# 
-#         Returns
-#         -------
-#         K_diag : array, shape (n_samples_X,)
-#             Diagonal of kernel k(X, X)
-#         """
-#         return np.ones(X.shape[0])
-# =============================================================================
 
     def is_stationary(self):
         return False
-    
-# =============================================================================
-#     def __repr__(self):
-#         return "{0:.3g}**2".format(np.sqrt(self.zeta))        
-# =============================================================================
-        
+            
     @property
     def hyperparameter_zeta(self):
+        """Correlation parameterized as: C[i,j] = exp(-(zeta[i]+zeta[j])"""
         return  Hyperparameter(name="zeta", 
                                value_type="numeric", 
                                bounds=self.zeta_bounds, 
                                n_elements=self.dim,
                                log=False)
 
+# =============================================================================
+# TODO: migrate most of this code to UnrestrictiveCorrelation
+# should be able to test: correlation = L*L.T
+# L -> C, S -> zeta
+# and conversely:
+# zeta -> L -> L*L.T = correlation
+# =============================================================================
     def unrestrictive_correlation(self):
-        """Calculate parameters to initialize UnrestrictiveCorrelation kernel"""
-        # assume all entries are positive, will be true if produced by MC model
+        """Calculate parameters to initialize UnrestrictiveCorrelation kernel.
+        
+        Assume all zeta values are positive, which will be true if produced by
+        MultiplicativeCorrelation model."""
 
         t = np.array(self.zeta, dtype=np.float64)
         t = np.exp(-t)
@@ -528,19 +537,25 @@ class MultiplicativeCorrelation(NormalizedKernelMixin, Kernel):
             prod = sqrt(1.0 - C[r,0]**2)
             S[r,0] = prod
             for s in range(1,r):
-                C[r,s] = L[r,s]/prod
+                C[r,s] = L[r,s] / prod
                 S[r,s] = sqrt(1.0 - C[r,s]**2)
                 prod *= S[r,s]
-            print("check: {} = {} : difference {}".format(L[r,r], prod, L[r,r] - prod))
+            #print("check: {} = {} : difference {}".format(L[r,r], prod, L[r,r] - prod))
         return np.arccos(C[np.tril_indices(dim, -1)])
 
 
 class Tensor(CompoundKernel):
     def __init__(self, kernels):
+        """Extends the product to a list of kernels.
+        
+        Typically use Projection to define kernels restricted to different
+        subsets of the coordinates, and combine them with Sum, Product,
+        Tensor, or DirectSum.  Parameter names will be much shorter
+        than if the Sum kernel operator + is used repeatedly."""
         super(Tensor, self).__init__(kernels)
         
     def __call__(self, X, Y=None, eval_gradient=False):
-        """Computes product of all the kernels (and gradients)"""
+        """Computes the product of a list of kernels (and their gradients)."""
         
         if eval_gradient:
             def _k_g_mul_(kg0, kg1):
@@ -561,10 +576,16 @@ class Tensor(CompoundKernel):
     
 class DirectSum(CompoundKernel):
     def __init__(self, kernels):
+        """Extends the sum to a list of kernels.
+        
+        Typically use Projection to define kernels restricted to different
+        subsets of the coordinates, and combine them with Sum, Product,
+        Tensor, or DirectSum.  Parameter names will be much shorter
+        than if the Sum kernel operator + is used repeatedly."""
         super(DirectSum, self).__init__(kernels)
         
     def __call__(self, X, Y=None, eval_gradient=False):
-        """Computes product of all the kernels (and gradients)"""
+        """Computes the sum of a list of kernels (and their gradients)."""
         
         if eval_gradient:
             def _k_g_add_(kg0, kg1):
@@ -625,7 +646,7 @@ if __name__ == "__main__":
 #     print(sk.hyperparameters)
 # =============================================================================
     
-    prod = ltk4 * ltk4
+    prod = ltk * ltk4
     print(prod.get_params(deep=False))
     print(prod.hyperparameters)
     
