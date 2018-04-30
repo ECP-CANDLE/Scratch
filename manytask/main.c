38515dd87fdd61f871107e5fc9d7c473d3f80d55
Justin Wozniak <wozniak@mcs.anl.gov>
2018-04-30 14:16:10 -0500

    Add time

--- manytask/main.c
+++ manytask/main.c
@@ -30,6 +30,8 @@ main(int argc, char* argv[])
 {
   MPI_Init(0, 0);
 
+  double start = MPI_Wtime();
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
@@ -47,6 +49,10 @@ main(int argc, char* argv[])
     master(n, workers);
   else
     worker();
+
+  double stop = MPI_Wtime();
+  if (rank == 0)
+    printf("TIME: %0.3f\n", stop-start);
 
   MPI_Finalize();
   return 0;
