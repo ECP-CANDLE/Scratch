e630f9101fbfec69cd949b7b0e8277d3223471cb
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-06-27 14:30:59 -0500

    Use new Swift/T, TURBINE_STDOUT

--- manytask/run-swift-theta.sh
+++ manytask/run-swift-theta.sh
@@ -3,7 +3,8 @@ set -eu
 
 # PATH=/home/wozniak/sfw/theta/swift-t-mt/stc/bin:$PATH
 # PATH=/projects/Candle_ECP/swift/2018-06-05/stc/bin:$PATH
-PATH=/projects/Candle_ECP/swift/2018-06-14/stc/bin:$PATH
+# PATH=/projects/Candle_ECP/swift/2018-06-14/stc/bin:$PATH
+PATH=/projects/Candle_ECP/swift/2018-06-20/stc/bin:$PATH
 
 which swift-t
 
@@ -17,11 +18,15 @@ export QUEUE=${QUEUE:-debug-flat-quad}
 # export TURBINE_APP_RETRIES=3
 export TURBINE_LOG=1
 
+echo QUEUE=$QUEUE
+
 set -x
 swift-t -m theta \
         -n $PROCS \
         -t w \
         -e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
-        -e TURBINE_APP_RETRIES_REPUT=3 \
-        -e TURBINE_LOG_RANKS=1 \
+        -e TURBINE_STDOUT="f-%%r.txt" \
         $*
+
+# -e TURBINE_APP_RETRIES_REPUT=3
+#         -e TURBINE_LOG_RANKS=1
