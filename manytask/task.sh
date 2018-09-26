b511b66a8d15203b4274773964208507e4cc7954
Justin Wozniak <woz@anl.gov>
2018-09-26 16:22:39 -0500

    More container ideas

--- manytask/task.sh
+++ manytask/task.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo TASK.SH rank=$ADLB_RANK_SELF $*
+
+CONTAINER=( /lustre/atlas/world-shared/stf007/ForArvind/arvind4_ppc64.img
+            --bind $HOME:$HOME )
+
+# Interactive:
+# singularity shell ${CONTAINER[@]}
+
+# One shot:
+# singularity exec ${CONTAINER[@]} bash subtask.sh
+
+# Real fast:
+# singularity exec ${CONTAINER[@]} bash -c exit
