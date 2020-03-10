8dcdbcfb99d147e69b36b234046ff5f860ab835f
Justin Wozniak <woz@anl.gov>
2020-03-10 15:50:28 -0500

    Draft of test-file - WIP

---
+++ stat-test/test-file-1.py
@@ -0,0 +1,69 @@
+
+"""
+
+TEST FILE 1
+
+Statement:
+A tumor type (cell type) produces more false negatives compared to
+true positives than others.
+
+Solution:
+Compute the z-score of the tumor type and its corresponding p-value
+at a significance level of 0.05 .
+"""
+
+import pprint
+
+from stat_tests import Result, significance_test
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description=
+                                     "Run a significance test.")
+    parser.add_argument("data",
+                        help="The data TSV file.")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Be verbose.")
+    args = parser.parse_args()
+    argv = vars(args)
+    return argv
+
+def get_XPs(filename):
+    """ Read/return the TPs and FPs """
+    results = {}
+    with open(filename) as fp:
+        line = fp.readline() # Discard header
+        while True:
+            line = fp.readline()
+            if len(line) == 0: break
+            p1 = line.find("\t")
+            if p1 < 0: raise Exception("Bad data!")
+            p2 = line.find("\t", p1+1)
+            if p2 < 0: raise Exception("Bad data!")
+            p3 = line.find("\t", p2+1)
+            if p3 < 0: raise Exception("Bad data!")
+            name = line[0:p1]
+            tps_s = line[p1+1:p2]
+            tps = int(float(tps_s))
+            fps_s = line[p2+1:p3]
+            fps = int(float(fps_s))
+            print(name, ":", tps, ":", fps)
+            results[name] = { Result.TP : tps,
+                              Result.FP : fps }
+    return results
+
+# # Generate some data
+# import random
+# tumors = []
+# for i in range(0, tumor_count):
+#     tumors.append({})
+#     tumors[i][Result.TP] = random.randint(0, trials)
+#     tumors[i][Result.FP] = trials - tumors[i][Result.TP]
+
+# significance_test(tumors)
+
+argv = parse_args()
+tumors = get_XPs(argv["data"])
+
+
+# pprint tumors
