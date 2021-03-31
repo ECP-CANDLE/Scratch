d59aff3e3c3bfd7dcbc1c3c740392b277926aa86
Justin Wozniak <woz@anl.gov>
2021-03-31 13:16:05 -0500

    More test scripts

---
+++ explainers/cf_fake.py
@@ -0,0 +1,24 @@
+
+# CF FAKE PY
+# Test that loading TF, etc., work as intended
+
+print("CF_FAKE: Starting imports ...")
+
+import tensorflow as tf
+tf.get_logger().setLevel(40) # suppress deprecation messages
+tf.compat.v1.disable_v2_behavior() # disable TF2 behaviour as alibi code still relies on TF1 constructs
+from tensorflow.keras.models import Model, load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from time import time
+from alibi.explainers import CounterFactual, CounterFactualProto
+#print('TF version: ', tf.__version__)
+#print('Eager execution enabled: ', tf.executing_eagerly()) # False
+import pickle
+
+
+print("CF_FAKE: Imports OK.")
+
+def run():
+    print("CF_FAKE.run(): OK.")
---
+++ explainers/one-shot.swift
@@ -0,0 +1,4 @@
+
+import python;
+
+python_persist("import cf_fake as cf", "cf.run()");
